fb0abb547b5d6633e0029c55ca0b93939429cd45
Henning Kvinnesland <henning@keyteq.no>
2014-11-23 17:57:36 +0100

    More completions.

---
+++ readme.md
@@ -0,0 +1,20 @@
+Provides code-completion for the eZ-API
+---------------------------------------
+
+Installation:
+-------------
+1. Install PhpStorm plugin.
+2. Install Php-bundle (does not exist yet)
+
+Usage:
+------
+Provides completion for the ContentTypeService
+
+Todo:
+* Validate location of console-executable (consoleProvider)
+* Decide on a format for data-exchange between console-command and CompletionProvider
+* Id-completions must remove string-literal on completion.
+* Create separate bundle for php-code.
+
+
+
--- src/eZCompletionContributor.java
+++ src/eZCompletionContributor.java
@@ -1,5 +1,6 @@
 import com.intellij.codeInsight.completion.*;
 import com.intellij.codeInsight.lookup.LookupElement;
+import com.intellij.codeInsight.lookup.LookupElementPresentation;
 import com.intellij.openapi.project.Project;
 import com.intellij.patterns.PlatformPatterns;
 import com.intellij.psi.PsiElement;
@@ -8,40 +9,54 @@ import org.jetbrains.annotations.NotNull;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class eZCompletionContributor extends CompletionContributor
 {
-    protected static ArrayList<String> cache;
+    protected static HashMap<String, ArrayList<String>> cache;
+    protected static HashMap<String, String> completionTypes;
+
+    protected static void setCompletionTypes()
+    {
+        completionTypes = new HashMap<>();
+        completionTypes.put("loadContentTypeGroup", "contentclassgroupid");
+        completionTypes.put("loadContentTypeGroupByIdentifier", "contentclassgroup");
+        completionTypes.put("loadContentTypeByIdentifier", "contentclass");
+        completionTypes.put("loadContentType", "contentclassid");
+    }
 
     public eZCompletionContributor()
     {
+        cache = new HashMap<>();
+        setCompletionTypes();
+
         extend(CompletionType.BASIC, PlatformPatterns.psiElement(), new CompletionProvider<CompletionParameters>()
         {
             @Override
             protected void addCompletions(@NotNull CompletionParameters parameters, ProcessingContext context, @NotNull CompletionResultSet result)
             {
-                PsiElement element = parameters.getPosition();
-                String lookupType = getLookupType(element, "loadContentTypeByIdentifier");
-                if (lookupType.length() == 0) {
-                    return;
-                }
-
                 Project project = parameters.getEditor().getProject();
                 if (project == null) {
                     return;
                 }
+
+                String lookupIdentifier = getLookupType(parameters.getPosition());
+                if (!completionTypes.containsKey(lookupIdentifier)) {
+                    return;
+                }
+                String lookupType = completionTypes.get(lookupIdentifier);
+
                 String baseDir = project.getBaseDir().getCanonicalPath();
-                performLookup(result, baseDir);
+                performLookup(result, baseDir, lookupType);
             }
         });
     }
 
-    protected void performLookup(CompletionResultSet result, String baseDir)
+    protected void performLookup(CompletionResultSet result, String baseDir, String type)
     {
-        if (cache == null) {
-            cache = new ArrayList<>();
-
-            String command = baseDir + "/../kavli.com/ezpublish/console devtools:contentclass";
+        if (!cache.containsKey(type)) {
+            cache.put(type, new ArrayList<String>());
+            String command = baseDir + "/../kavli.com/ezpublish/console code:completion --type=" + type;
 
             Process process;
             try {
@@ -49,22 +64,29 @@ public class eZCompletionContributor extends CompletionContributor
                 process.waitFor();
                 BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
 
-                String contentClass;
-                while ((contentClass = reader.readLine())!= null) {
-                    cache.add(contentClass);
-                }
-
+                String completion;
+                while ((completion = reader.readLine())!= null) {
+                    cache.get(type).add(completion);
+            }
             } catch (Exception e) {
                 return;
             }
         }
 
-        for (String completion : cache) {
-            result.addElement(new eZCompletion(completion));
+        for (String completion : cache.get(type)) {
+            // Contains id as well.
+            if (completion.contains(";")) {
+                String[] parts = completion.split(";");
+                result.addElement(new eZCompletion(parts[0], parts[1]));
+            }
+            else {
+                result.addElement(new eZCompletion(completion));
+            }
         }
+
     }
 
-    protected String getLookupType(PsiElement element, String searchText)
+    protected String getLookupType(PsiElement element)
     {
         PsiElement method = element
                 .getParent()
@@ -74,32 +96,35 @@ public class eZCompletionContributor extends CompletionContributor
                 .getNextSibling()
                 .getNextSibling();
 
-        if (method == null) {
-            return "";
-        }
-
-        String identifier = method.getText();
-        if (!identifier.equals(searchText)) {
-            return "";
-        }
-
-        return identifier;
+        return method == null ? "" : method.getText();
     }
 }
 
 class eZCompletion extends LookupElement
 {
-    protected String entry;
+    protected String value;
+    protected String displayText;
 
-    public eZCompletion(String lookupEntry)
+
+    public eZCompletion(String value, String displayText)
     {
-        entry = lookupEntry;
+        this.value = value;
+        this.displayText = displayText;
+    }
+
+    public eZCompletion(String entry)
+    {
+        value = entry;
+        displayText = entry;
     }
 
     @NotNull
     @Override
-    public String getLookupString()
+    public String getLookupString() { return value; }
+
+    @Override
+    public void renderElement(LookupElementPresentation presentation)
     {
-        return entry;
+        presentation.setItemText(displayText);
     }
 }
