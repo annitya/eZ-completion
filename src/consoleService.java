ce691465f67ebdf3f5317585d99f759e8801a89a
Henning Kvinnesland <henning@keyteq.no>
2014-11-26 11:55:17 +0100

    Updated compiled version.

---
+++ src/consoleService.java
@@ -0,0 +1,41 @@
+import com.google.gson.Gson;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManager;
+import com.sun.xml.internal.xsom.impl.Ref;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.util.concurrent.Callable;
+
+public class ConsoleService implements Callable
+{
+    protected static String getConsole() throws Exception
+    {
+        Project project = ProjectManager.getInstance().getOpenProjects()[0];
+        String path = project.getBaseDir().getCanonicalPath() + File.separator + "ezpublish/console";
+        if (!new File(path).isFile()) {
+            throw new Exception("Unable to locate console-executable");
+        }
+
+        return path;
+    }
+
+    @Override
+    public CompletionContainer call() throws Exception
+    {
+        String command = getConsole() + " ezcode:completion";
+
+        Process process = Runtime.getRuntime().exec(command);
+        process.waitFor();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String json = "", currentLine;
+        while ((currentLine = reader.readLine()) != null) {
+            json += currentLine;
+        }
+
+        Gson gson = new Gson();
+        return gson.fromJson(json, CompletionContainer.class);
+    }
+}
