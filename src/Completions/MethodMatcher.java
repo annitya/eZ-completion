8de6ca1a4d5409c2d4c2aa84f5fd386f08ff26f7
Henning Kvinnesland <henning@keyteq.no>
2014-12-21 21:48:23 +0100

    Module-view completion is now actually helpful.

--- src/Completions/MethodMatcher.java
+++ src/Completions/MethodMatcher.java
@@ -11,6 +11,7 @@ public class MethodMatcher extends PatternCondition<PsiElement>
 {
     protected int parameterIndex;
     protected String method;
+    protected String dependence;
 
     public MethodMatcher(int parameterIndex, String method)
     {
@@ -22,16 +23,17 @@ public class MethodMatcher extends PatternCondition<PsiElement>
     @Override
     public boolean accepts(@NotNull PsiElement psiElement, ProcessingContext context)
     {
+        return withinQuotes(psiElement) && dependenceMatches(psiElement) && methodMatches(psiElement) && parameterMatches(psiElement);
+    }
+
+    protected Boolean withinQuotes(PsiElement psiElement)
+    {
         // Require quotes to trigger completion.
         try {
-            if (((LeafPsiElement) psiElement).getElementType().toString().equals("identifier")) {
-                return false;
-            }
+            return !(((LeafPsiElement) psiElement).getElementType().toString().equals("identifier"));
         } catch (Exception e) {
             return false;
         }
-
-        return methodMatches(psiElement) && parameterMatches(psiElement);
     }
 
     protected Boolean methodMatches(PsiElement psiElement)
@@ -51,10 +53,21 @@ public class MethodMatcher extends PatternCondition<PsiElement>
         }
     }
 
-    public Boolean parameterMatches(PsiElement psiElement)
+    protected Boolean dependenceMatches(PsiElement psiElement)
+    {
+        if (dependence == null) {
+            return true;
+        }
+
+        PsiElement[] parameters = getParameters(psiElement);
+
+        return parameters.length > 0 && parameters[0].getText().replaceAll("^\"|\"$|\'|\'$", "").equals(dependence);
+    }
+
+    protected Boolean parameterMatches(PsiElement psiElement)
     {
         try {
-            PsiElement[] parameters = ((ParameterList)psiElement.getParent().getParent()).getParameters();
+            PsiElement[] parameters = getParameters(psiElement);
             for (int i=0; i < parameters.length; i++) {
                 if (parameters[i].equals(psiElement.getParent())) {
                     return i == parameterIndex;
@@ -65,6 +78,15 @@ public class MethodMatcher extends PatternCondition<PsiElement>
 
         } catch (Exception e) {
             return false;
+        }
+    }
+
+    protected PsiElement[] getParameters(PsiElement psiElement)
+    {
+        try {
+            return ((ParameterList)psiElement.getParent().getParent()).getParameters();
+        } catch (Exception e) {
+            return new PsiElement[0];
         }
     }
 }
