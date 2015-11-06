1d54d9141aaf7accb6724c24606c6ad65f6e05ba
Henning Kvinnesland <henning@keyteq.no>
2015-11-06 15:08:01 +0100

    Translation-type-provider now uses a slightly less weak matching-type.

--- src/TypeProviders/Content/Fields/TranslationFieldTypeProvider.java
+++ src/TypeProviders/Content/Fields/TranslationFieldTypeProvider.java
@@ -5,17 +5,25 @@ import com.jetbrains.php.lang.psi.elements.MethodReference;
 import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
 import org.jetbrains.annotations.Nullable;
 
+/**
+ * Provides type for fields translated by the translation-helper.
+ * $translationHelper->getTranslatedField($content, 'identifier')->|
+ */
 public class TranslationFieldTypeProvider extends FieldTypeProvider
 {
     @Nullable
     @Override
     public String resolveType(PsiElement psiElement)
     {
-        // Unless fqn is matched, this will trigger the field-type-provider and cause a npe.
         MethodReference methodReference;
         try {
             methodReference = (MethodReference)psiElement;
         } catch (Exception e) {
+            return null;
+        }
+        String methodName = methodReference.getName();
+        // Slightly weak match.
+        if (methodName == null || !methodName.equals("getTranslatedField")) {
             return null;
         }
 
