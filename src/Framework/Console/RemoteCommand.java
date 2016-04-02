d0c86c875d83bc9c5015976d79512b78d48bb188
Henning Kvinnesland <henning@byte.no>
2016-04-02 22:59:56 +0200

    Adaptations for latest PhpStorm-version.

--- src/Framework/Console/RemoteCommand.java
+++ src/Framework/Console/RemoteCommand.java
@@ -6,20 +6,22 @@ import com.intellij.remote.RemoteSdkAdditionalData;
 import com.intellij.remote.RemoteSdkCredentials;
 import com.jetbrains.php.config.commandLine.PhpCommandSettings;
 import com.jetbrains.php.remote.PhpRemoteProcessUtil;
+import com.jetbrains.plugins.remotesdk.RemoteSdkUtil;
+import com.jetbrains.plugins.remotesdk.transport.SshRemoteSession;
 
 
 public class RemoteCommand
 {
     public static Process createProcess(PhpCommandSettings commandSettings, Project project) throws Exception
     {
-
         GeneralCommandLine generalCommandLine = commandSettings.createGeneralCommandLine();
         RemoteSdkAdditionalData remoteSdkAdditionalData = (RemoteSdkAdditionalData)commandSettings.getAdditionalData();
         if (remoteSdkAdditionalData == null) {
             throw new Exception("Unable to fetch remote sdk-data!");
         }
 
-        RemoteSdkCredentials remoteSdkCredentials = remoteSdkAdditionalData.getRemoteSdkCredentials(false);
-        return PhpRemoteProcessUtil.createRemoteProcess(project, remoteSdkCredentials, generalCommandLine, true);
+        RemoteSdkCredentials remoteSdkCredentials = remoteSdkAdditionalData.getRemoteSdkCredentials(project, false);
+        SshRemoteSession session = RemoteSdkUtil.createRemoteSession(project, remoteSdkCredentials);
+        return RemoteSdkUtil.createRemoteProcess(session, generalCommandLine, false, false, true);
     }
 }
